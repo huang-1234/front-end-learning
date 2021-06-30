534bb3cf538a5a18dbf2bce6e0b4868c3ee84793
hsq <137707531@qq.com>
2021-06-30 23:36:31 +0800

    update cso

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
+
 set -e
 
-git add .
-git commit -m 'first commit'
 
-git push
+git add .
+git commit -m 'update cso'
+
+git remote -v
+
+git push -f git@github.com:huang-1234/front-end-learning.git master
